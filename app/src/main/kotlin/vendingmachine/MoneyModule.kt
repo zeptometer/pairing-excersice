fc5085643e4e0cd4169fa6e0148021b84e20012a
Yuito <yuito@acupof.coffee>
2022-10-29 14:52:33 +0900

    特定の金額から貨幣の個数のマップを作ると、貨幣の合計金額が、特定の金額に一致する

--- app/src/main/kotlin/vendingmachine/MoneyModule.kt
+++ app/src/main/kotlin/vendingmachine/MoneyModule.kt
@@ -6,4 +6,18 @@ object MoneyModule {
     fun calculateAmount(moneyMap: Map<Money, Int>): Int {
         return moneyMap.entries.sumOf { (money, num) -> money.value * num }
     }
+
+    fun toMoneyCount(amount: Int, supportedMoneySet: Set<Money>): Map<Money, Int> {
+        var rest = amount
+        val moneyCount = mutableMapOf<Money, Int>()
+        for (money in supportedMoneySet.sortedBy { -it.value }) {
+            val count = rest / money.value;
+            rest %= money.value
+            moneyCount[money] = count
+        }
+        if (rest != 0) {
+            throw IllegalArgumentException()
+        }
+        return moneyCount.toMap()
+    }
 }
--- app/src/test/kotlin/vendingmachine/MoneyModuleTest.kt
+++ app/src/test/kotlin/vendingmachine/MoneyModuleTest.kt
@@ -3,6 +3,7 @@ package vendingmachine
 import org.junit.jupiter.api.Test
 import vendingmachine.model.Money
 import kotlin.test.assertEquals
+import kotlin.test.assertFails
 
 internal class MoneyModuleTest {
     @Test
@@ -31,5 +32,50 @@ internal class MoneyModuleTest {
                 )
             )
         )
+    }
+
+    @Test
+    fun `金額から貨幣の個数のマップを作る`() {
+        val supportedMoneySet = setOf(Money.Ten, Money.Fifty, Money.Hundred, Money.FiveHundred, Money.Thousand)
+
+        assertEquals(
+            mapOf(
+                Money.Ten to 4,
+                Money.Fifty to 1,
+                Money.Hundred to 1,
+                Money.FiveHundred to 1,
+                Money.Thousand to 2
+            ),
+            MoneyModule.toMoneyCount(2690, supportedMoneySet)
+        )
+
+        assertEquals(
+            mapOf(
+                Money.Ten to 1,
+                Money.Fifty to 0,
+                Money.Hundred to 2,
+                Money.FiveHundred to 0,
+                Money.Thousand to 3
+            ),
+            MoneyModule.toMoneyCount(3210, supportedMoneySet)
+        )
+
+        assertEquals(
+            mapOf(
+                Money.Ten to 0,
+                Money.Fifty to 0,
+                Money.Hundred to 0,
+                Money.FiveHundred to 0,
+                Money.Thousand to 5
+            ),
+            MoneyModule.toMoneyCount(5000, supportedMoneySet)
+        )
+    }
+
+    @Test
+    fun `サポートしている貨幣から貨幣の個数のマップを作れない時エラーを出す`() {
+        val supportedMoneySet = setOf(Money.Ten, Money.Fifty, Money.Hundred, Money.FiveHundred, Money.Thousand)
+
+        assertFails { MoneyModule.toMoneyCount(1, supportedMoneySet) }
     }
 }
